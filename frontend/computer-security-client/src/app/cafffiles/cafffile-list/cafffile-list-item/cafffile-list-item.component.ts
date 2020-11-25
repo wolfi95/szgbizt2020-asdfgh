5b87b098421fc3a56e04d4392f82f9c501570d2d
Panna Somlo <psomlo97@gmail.com>
2020-11-25 21:31:38 +0100

    Update image preview

--- frontend/computer-security-client/src/app/cafffiles/cafffile-list/cafffile-list-item/cafffile-list-item.component.ts
+++ frontend/computer-security-client/src/app/cafffiles/cafffile-list/cafffile-list-item/cafffile-list-item.component.ts
@@ -1,5 +1,6 @@
 import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ICaffFileListItemModel } from 'src/app/core/models/cafffile';
 
 @Component({
@@ -8,12 +9,15 @@ import { ICaffFileListItemModel } from 'src/app/core/models/cafffile';
   styleUrls: ['./cafffile-list-item.component.scss']
 })
 export class CafffileListItemComponent implements OnInit {
-
+  imageSource: any;
   @Input() caffFile: ICaffFileListItemModel;
   
-  constructor() { }
+  constructor(
+    private sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit(): void {
+    this.imageSource = this.sanitizer.bypassSecurityTrustResourceUrl(`data:image/png;base64, ${this.caffFile.imageData}`);
   }
 
 }
