57b3ad83d428bbdde8bfc004d490dca0d3401e0d
Marton Valki <marton.valki@verint.com>
2020-11-26 22:00:09 +0100

    change parseCaffToBmpStreamV1 definition and itroduce basic logging

--- caff_parser/caff_parser/main.h
+++ caff_parser/caff_parser/main.h
@@ -11,7 +11,7 @@ extern "C" {
 #  define MODULE_API
 #endif
 	MODULE_API int Test(int i);
-	MODULE_API char* parseCaffToBmpStreamV1(const char* caffName, long& fileLength);
+	MODULE_API char* parseCaffToBmpStreamV1(const char* caffName);
 #ifdef __cplusplus
 }
 #endif
